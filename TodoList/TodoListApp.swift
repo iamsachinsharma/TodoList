b6b3509494bcd323d2b76ac756804114080195fe
Sachin <sachinsharma2168@gmail.com>
2023-07-05 19:39:51 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Sachin Sharma on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
